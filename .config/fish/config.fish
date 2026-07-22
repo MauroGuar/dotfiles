d0aa0b1324ed47889c4830e5125dade8c7e90927
MauroGuar <m.guarnieri@alumno.um.edu.ar>
2026-07-22 13:12:10 -0300

    Fish vi mode.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,5 @@ set fish_greeting
 alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
 end
